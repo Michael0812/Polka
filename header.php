0964d126027e3bd749e4803c8388ef0b7e203b93
Michael0812 <michalwesolowski11@gmail.com>
2023-03-15 21:58:43 +0000

    style testomonials section & add new font

--- header.php
+++ header.php
@@ -10,7 +10,9 @@
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
-
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
 
