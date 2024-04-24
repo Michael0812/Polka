16ba09b9e7477d3f373a7f9317e6a595312ec7c4
John Doe <michalwesolowski11@gmail.com>
2024-04-25 00:48:21 +0100

    add fonts, styling and fucntionality for services

--- js/main.js
+++ js/main.js
@@ -1,56 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function() {
+    var slideItems = $('.slider-card');
+    var container = $('.content-card');
+    var activeItem = 0;
+    var move = 0;
+
+    var leftArrow = $('.left-arrow');
+    var rightArrow = $('.right-arrow');
+
+    // Left arrow click
+    leftArrow.click(function() {
+        activeItem--;
+        if (activeItem < 0) {
+            activeItem = slideItems.length - 1; // Go to last slide
+        }
+        move = activeItem * -0.8;
+        container.css('transform', 'translate(' + (move * 100) + '%)');
+    });
+
+    // Right arrow click
+    rightArrow.click(function() {
+        activeItem++;
+        if (activeItem >= slideItems.length) {
+            activeItem = 0; // Go to first slide
+        }
+        move = activeItem * -0.8;
+        container.css('transform', 'translate(' + (move * 100) + '%)');
+    });
+
+    // Swipe left
+    container.on('swipeleft', function() {
+        rightArrow.click(); // Trigger right arrow click
+    });
+
+    // Swipe right
+    container.on('swiperight', function() {
+        leftArrow.click(); // Trigger left arrow click
+    });
     (function () {
-        var listItems = document.querySelectorAll('#subtitle-list .subtitle');
-        var displayTextElement = document.getElementById('display-text');
-        var nextButtons = document.querySelectorAll('.next-button'); // Define nextButtons here
-        var pages = document.querySelectorAll('.page'); // Define pages here
-
-        if (listItems.length === 0) {
-            console.error('No list items found');
-            return;
-        }
-
-        var activeListItem = listItems[0]; // The first li element is active by default
-
-        // Make the first li element active by default
-        if (activeListItem.querySelector('a')) {
-            activeListItem
-                .querySelector('a')
-                .style
-                .color = '#7224B3';
-        }
-
-        if (activeListItem.querySelector('.hidden-info')) {
-            displayTextElement.innerHTML = activeListItem
-                .querySelector('.hidden-info')
-                .innerHTML;
-        }
-
-        listItems.forEach(function (listItem) {
-            listItem.addEventListener('mouseover', function () {
-                var hiddenInfo = listItem.querySelector('.hidden-info');
-                if (hiddenInfo) {
-                    displayTextElement.innerHTML = hiddenInfo.innerHTML;
-                }
-
-                if (activeListItem.querySelector('a')) {
-                    activeListItem
-                        .querySelector('a')
-                        .style
-                        .color = ''; // Reset the color of the previously active li element
-                }
-
-                if (listItem.querySelector('a')) {
-                    listItem
-                        .querySelector('a')
-                        .style
-                        .color = '#7224B3'; // Make the hovered li element active
-                }
-
-                activeListItem = listItem; // Update the active li element
-            });
-        });
-
         nextButtons.forEach(function (button, index) {
             button.addEventListener('click', function () {
                 var inputs = Array.from(
